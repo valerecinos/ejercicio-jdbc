6cd024bd109085d21781a05d3736000dffaa8d56
valerecinos <124801457+valerecinos@users.noreply.github.com>
2023-09-04 22:57:15 -0600

    Create Proveedores.java

---
+++ Proveedores.java
@@ -0,0 +1,15 @@
+package com.mycompany.proveedores;
+
+/**
+ *
+ * @author valer
+ */
+public class Proveedores {
+
+    public static void main(String[] args) {
+        FormProveedores objetoVista=new FormProveedores();
+        objetoVista.setVisible(true);
+        FormProductos objetoVista2=new FormProductos();
+        objetoVista2.setVisible(true);
+    }
+}
